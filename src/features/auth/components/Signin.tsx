17e5d597916b5b6bc4529c95e98fa200eac10342
Saketben10 <madmansaketkumar@gmail.com>
2025-08-31 23:15:14 +0530

    added hookform

--- src/features/auth/components/Signin.tsx
+++ src/features/auth/components/Signin.tsx
@@ -1,13 +1,36 @@
 "use client";
-
+import { zodResolver } from "@hookform/resolvers/zod";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { create } from "domain";
+import {
+  Form,
+  FormItem,
+  FormField,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
 
 export const Signin = () => {
+  const formSchema = z.object({
+    email: z.email(),
+    password: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <Card className="w-full h-full md:w-[487px]  border-none shadow-md">
       <CardHeader className="flex items-center justify-center text-center p-7">
@@ -18,29 +41,38 @@ export const Signin = () => {
       </div>
 
       <CardContent className="p-7">
-        <form className="space-y-4">
-          <Input
-            required
-            placeholder="enter your email"
-            value={""}
-            onChange={() => {}}
-            disabled={false}
-            type="email"
-          />
-          <Input
-            required
-            placeholder="enter password"
-            value={""}
-            onChange={() => {}}
-            disabled={false}
-            type="password"
-            min={3}
-            max={256}
-          />
-          <Button size={"lg"} disabled={false} className="w-full">
-            Login
-          </Button>
-        </form>
+        <Form {...form}>
+          <form className="space-y-4">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <Input
+                    placeholder="enter your email"
+                    type="email"
+                    {...field}
+                  />
+                  ;
+                </FormItem>
+              )}
+            />
+
+            <Input
+              required
+              placeholder="enter password"
+              value={""}
+              onChange={() => {}}
+              disabled={false}
+              type="password"
+              min={3}
+              max={256}
+            />
+            <Button size={"lg"} disabled={false} className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
       </CardContent>
       <div className="px-7">
         <DottedSeparator />
